eeef9b63a3fcf8286464c33f5d25f6dfe919d76c
SOWBARANIKA ESWARI <123854536+sowbi30@users.noreply.github.com>
2023-09-26 09:25:03 +0530

    Add files via upload

--- script1.js
+++ script1.js
@@ -1,26 +1,15 @@
-// XML - HTTP REQUEST: it is used to interact with servers via API.
-//Why: to get the data from the server via API.
+const flagsContainer = document.getElementById("flags-container");
 
-//Step 01: Create a XHR Objects
-//var variableName: userdefined your choice
-var request = new XMLHttpRequest();
-//Step 02: Specifiy the API
-request.open("GET", "https://restcountries.com/v2/all");
-//Step 03: Sending the request
-request.send();
-//Step 04: Once the requested data is there in server once data is successfully loaded,
-//Server response will be of 200 status code.
-//Functions: they are used to perform specific task.
-request.onload=function(){
-    var result = JSON.parse(request.response);
-    for(var flags in result){
-        console.log(flags,result[flags]);
-    }
-   
-    //whatever conversion has to be done here only
-    //not outside
-}
-
-//inside the func: we are converting the
-// data from server to the client
-//which is in string -> object.
+fetch("https://restcountries.com/v2/all")
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((country) => {
+      const flagElement = document.createElement("img");
+      flagElement.src = country.flags[0];
+      flagElement.alt = country.name;
+      flagsContainer.appendChild(flagElement);
+    });
+  })
+  .catch((error) => {
+    console.error("Error fetching data:", error);
+  });
